0aa67a48e4c37d6b3e1b2fdb8c6ea6dadac2bac5
mai9123 <mai.ito0717@gmail.com>
2025-06-27 18:56:51 +0900

    Initial Commit

---
+++ CareerLog/CareerLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareerLogApp.swift
+//  CareerLog
+//
+//  Created by 伊藤真依 on 2025/06/27.
+//
+
+import SwiftUI
+
+@main
+struct CareerLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
